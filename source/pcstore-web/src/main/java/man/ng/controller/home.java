646918c60691136f24f868ab957d46ad8f7a49e6
n2m2912 <mannguyen281129@gmail.com>
2018-09-23 19:11:00 +0700

    add login,register modal, edit card, image modal, image hover.

--- source/pcstore-web/src/main/java/man/ng/controller/home.java
+++ source/pcstore-web/src/main/java/man/ng/controller/home.java
@@ -8,7 +8,18 @@ import org.springframework.web.bind.annotation.RequestMethod;
 public class home {
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
-    public final String home() {
+    public final String index() {
         return "home/home";
     }
+    
+    @RequestMapping(value = "/detail", method = RequestMethod.GET)
+    public final String detail() {
+        return "detail/detail";
+    }
+    
+    @RequestMapping(value = "/detail-type", method = RequestMethod.GET)
+    public final String detailType() {
+        return "detail-type/detail-type";
+    }
+    
 }
