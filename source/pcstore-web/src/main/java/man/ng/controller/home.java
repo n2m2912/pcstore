c6995d5dce424e9f06100507aea2dc7624b6803e
n2m2912 <mannguyen281129@gmail.com>
2018-09-27 09:41:02 +0700

    add cart layout

--- source/pcstore-web/src/main/java/man/ng/controller/home.java
+++ source/pcstore-web/src/main/java/man/ng/controller/home.java
@@ -41,4 +41,9 @@ public class home {
     public final String shipPolicy() {
         return "ship-policy/ship-policy";
     }
+    
+    @RequestMapping(value = "/cart", method = RequestMethod.GET)
+    public final String cart() {
+        return "cart/cart";
+    }
 }
